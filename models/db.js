47e8dad73def837ae5358b2f4d9e37e2411c1cec
Akash More <akash@vempower.org>
2025-05-17 15:34:49 +0530

    .env setup for config

--- models/db.js
+++ models/db.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+dotenv.config();
+
 export const connectDB = () => {
-    mongoose.connect("mongodb://localhost:27017/Todo");
+    // mongoose.connect("mongodb://localhost:27017/Todo");
+    console.log(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI);
 }
